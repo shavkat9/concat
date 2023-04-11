428e12667700fb702369289e3088e3da450daae1
Shavkat Berdibekov <103271898+shavkat9@users.noreply.github.com>
2023-04-11 17:26:31 +0500

    Js concat

---
+++ Новая папка (2)/app.js
@@ -0,0 +1,26 @@
+// let a = 40;
+// let b = 50;
+// let g = 30;
+
+// let c = a - b + g;
+
+// console.log(c)
+
+// let a = 10;
+// let b = a;
+// a = 20
+// console.log(a, b)
+// const fruits = ["Banana", "Orange", "Apricot", "Mango"];
+
+// let fruit = fruits.at(2);
+
+// console.log(fruit)
+// const arr1 = ["Cecilie", "Lone"];
+// const arr2 = ["Emil", "Tobias", "Linus"];
+// const children = arr1.concat(arr2);
+
+// console.log(children)
+const arr1 = ["Cecilie", "Lone"];
+const arr2 = ["Emil", "Tobias", "Linus"];
+const arr3 = ["Robin4444444"];
+console.log(  arr1.concat(arr2, arr3));
